138bcfc3a73d683308e7b25b703fe84bf2f512d7
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-04-23 07:24:18 +0900

    ボイラープレート削除

---
+++ src/data.js
@@ -0,0 +1,35 @@
+const questions = [
+  {
+    id: 1,
+    question: "Who is the President of the United States ?",
+    choiceA: "Donald Trump",
+    choiceB: "Hillary Clinton",
+    choiceC: "Barack Obama",
+    choice: "Joe Biden",
+  },
+  {
+    id: 2,
+    question: "Who is the President of the United States ?",
+    choiceA: "Donald Trump",
+    choiceB: "Hillary Clinton",
+    choiceC: "Barack Obama",
+    choice: "Joe Biden",
+  },
+  {
+    id: 3,
+    question: "Who is the President of the United States ?",
+    choiceA: "Donald Trump",
+    choiceB: "Hillary Clinton",
+    choiceC: "Barack Obama",
+    choice: "Joe Biden",
+  },
+  {
+    id: 4,
+    question: "Who is the President of the United States ?",
+    choiceA: "Donald Trump",
+    choiceB: "Hillary Clinton",
+    choiceC: "Barack Obama",
+    choice: "Joe Biden",
+  },
+];
+export default questions;
