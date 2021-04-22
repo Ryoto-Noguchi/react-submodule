13f4d95657fcb817cf26ca0874db9b873c2e66f0
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-04-23 08:37:16 +0900

    React-Bootstarpを使用できるように変更

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React, { useState } from "react";
 import data from "./data";
-
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 function App() {
   const [questions, setQuestions] = useState(data);
-  return <main>
-    <div className="container">
-      <div className="question"></div>
-    </div>
-  </main>;
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const question = questions.id === questionNumber;
+  // const currentQuestionNumber = questions.find((question) => question.id === 1);
+  // setQuestionNumber(currentQuestionNumber)
+  return (
+    <main>
+      <div className="container">
+        <div className="question">
+          <h2>Here is a line for a question</h2>
+        </div>
+        <div className="choices">
+          <Row>
+            <Col xs={6}>
+              Here is a box for choice
+              <Button variant="primary">Button</Button>
+            </Col>
+            <Col xs={6}>
+              Here is a box for choice
+              <Button variant="primary">Button</Button>
+            </Col>
+            <Col xs={6}>
+              Here is a box for choice
+              <Button variant="primary">Button</Button>
+            </Col>
+            <Col xs={6}>
+              Here is a box for choice
+              <Button variant="primary">Button</Button>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    </main>
+  );
 }
+export default App;
