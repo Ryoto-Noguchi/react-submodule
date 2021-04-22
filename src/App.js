6ef116bee52f27768e8b7fb9ad19472892423968
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-04-23 07:40:51 +0900

    index.css追加

--- src/App.js
+++ src/App.js
@@ -1,2 +1,11 @@
 import React, { useState } from "react";
 import data from "./data";
+
+function App() {
+  const [questions, setQuestions] = useState(data);
+  return <main>
+    <div className="container">
+      <div className="question"></div>
+    </div>
+  </main>;
+}
