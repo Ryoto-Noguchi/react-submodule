81aed529a01fb31cba9fd0c09988a6ff84d8108f
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-04-25 21:15:36 +0900

    App.js変更

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   // const currentQuestionNumber = questions.find((question) => question.id === 1);
   // setQuestionNumber(currentQuestionNumber)
   return (
+    
     <main>
       <div className="container">
         <div className="question">
