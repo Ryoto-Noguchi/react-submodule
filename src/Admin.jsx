b9971b447c1115f78b2e6ba614148178a86d3594
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-05-10 08:43:47 +0900

    Adminの「Ready Go !」ボタンを押したときに値を取得することができない。useRef参照

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -3,6 +3,7 @@ import { Container, Row, Col, Button, Card, Table, Form } from "react-bootstrap"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // ↓font-awesomeのnameの指定の仕方(https://stackoverflow.com/questions/56351531/how-to-find-icon-names-for-font-awesome-to-import-with-react)
 import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import {countdown} from "./Monitor"
 
 const Admin = () => {
   return (
@@ -35,7 +36,7 @@ const Admin = () => {
             </h3>
             <div className="sound-btns">
               <Button>Question</Button>
-              <Button>Ready Go !</Button>
+              <Button id="ready-go-btn" onClick={() => countdown()}>Ready Go !</Button>
               <Button>Answer Check</Button>
             </div>
           </div>
--- src/Monitor.jsx
+++ src/Monitor.jsx
@@ -3,15 +3,18 @@ import data from "./data";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import axios from "axios";
 
-function Monitor() {
+export function Monitor() {
   const [questions, setQuestions] = useState(data);
   const [questionNumber, setQuestionNumber] = useState(1);
   const question = questions.find((question) => question.id === questionNumber);
+
+
   return (
     <main>
       <Container className="container">
         <div className="question-box">
           <p className="question-text">{question.question}</p>
+          <span id="countDown">10</span>
         </div>
         <Row className="row">
           <Col  className="choice-box">
@@ -58,6 +61,31 @@ function Monitor() {
       </Container>
     </main>
   );
+}
+
+export function countdown() {
+  console.log("ready go clicked")
+  let second = document.getElementById('countDown');
+  console.log(second)
+  // second.style.color = "white";
+  const count = 0;
+
+  const timerID = setInterval(function () {
+    if (second === count) {
+      clearInterval(timerID);
+      ringTimeUp();
+      ringAnswerCheck();
+      second.textContent = second--;
+    }
+  }, 1000);
+}
+
+export function ringTimeUp() {
+  console.log("Time's Up");
+}
+
+export function ringAnswerCheck() {
+  console.log("Answer Check");
 }
 
 export default Monitor;
