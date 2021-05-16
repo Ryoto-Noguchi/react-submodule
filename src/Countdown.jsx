e5606ca00dce5650caaa69e5b327bd7f5d545bf1
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-05-16 11:28:59 +0900

    カウントダウンコンポーネント作成完了

---
+++ src/Countdown.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useRef, useEffect } from "react";
+import { Button } from "react-bootstrap";
+import { ringTimeUp, ringAnswerCheck } from "./Monitor";
+
+function Countdown(props) {
+  let { number, setNumber } = props;
+  const [isPlaying, setIsPlaying] = useState(false);
+  const countdownAudioEl = useRef(null);
+
+  const countdown = () => {
+    console.log(number);
+    setIsPlaying((isPlaying) => !isPlaying);
+    setIsPlaying((isPlaying) => {
+      return isPlaying;
+    });
+
+    setIsPlaying(!isPlaying);
+    const count = 0;
+    const timerID = setInterval(function () {
+      if (number === count) {
+        clearInterval(timerID);
+        ringTimeUp();
+        ringAnswerCheck();
+      } else {
+        setNumber(--number);
+        console.log(number);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    console.log("played");
+    if (isPlaying) {
+      countdownAudioEl.current.play();
+    } 
+  }, [isPlaying]);
+
+  return (
+    <div>
+      <Button className="manupulate-btn" onClick={() => countdown()}>
+        Start
+      </Button>
+      <audio
+        // ↓「src="../public/music/countdown_10sec.mp3"」ではエラーになる。なぜだかは不明
+        src="./music/countdown_10sec.mp3"
+        ref={countdownAudioEl}
+      ></audio>
+    </div>
+  );
+}
+
+export default Countdown;
