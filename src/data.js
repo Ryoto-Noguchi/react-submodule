f51f4ec763f4af7df1c6695dc08559cd61fcd8b3
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-05-17 18:37:34 +0900

    テストデータ変更

--- src/data.js
+++ src/data.js
@@ -6,43 +6,53 @@ const questions = [
       A: "Donald Trump",
       B: "Hillary Clinton",
       C: "Joe Biden",
-      D: "Joe Biden"
+      D: "Barack Obama"
     },
-    answer: "C"
+    answer: "C",
   },
   {
     id: 2,
     question: "Who is the Prime Minister of Japan ?",
     choices: {
-      A: "Donald Trump",
-      B: "Hillary Clinton",
-      C: "Shinzo Abe",
-      D: "Yoshihide Suga"
+      A: "Shinjiro Koizumi",
+      B: "Junichiro Koizumi",
+      C: "Yoshihide Suga",
+      D: "Shinzo Abe"
     },
-    answer: "C"
+    answer: "D",
   },
   {
     id: 3,
-    question: "Who is the President of the United States ?",
+    question: "What country has the biggest poplulation in the world ?",
     choices: {
-      A: "Donald Trump",
-      B: "Hillary Clinton",
-      C: "Joe Biden",
-      D: "Joe Biden"
+      A: "China",
+      B: "India",
+      C: "the United States of America",
+      D: "Indonesia",
     },
-    answer: "C"
+    answer: "A",
   },
   {
     id: 4,
-    question: "Who is the President of the United States ?",
+    question: "Which country is located in Asia ?",
     choices: {
-      A: "Donald Trump",
-      B: "Hillary Clinton",
-      C: "Joe Biden",
-      D: "Joe Biden"
+      A: "Argentina",
+      B: "Brazil",
+      C: "Russia",
+      D: "Vietnam",
     },
-    answer: "C"
+    answer: "D",
   },
-
+  {
+    id: 5,
+    question: "Which country was founded in 1945 ?",
+    choices: {
+      A: "Jamaica",
+      B: "South Korea",
+      C: "Japan",
+      D: "China",
+    },
+    answer: "D",
+  },
 ];
 export default questions;
