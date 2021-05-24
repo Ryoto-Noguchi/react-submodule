0b7294cf6582e57cb165156a9d01316586843b36
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-05-25 08:27:07 +0900

    QuestionManagementコンポーネント作成

---
+++ src/component/admin/QuestionManagement.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Row, Col, Table, Button } from "react-bootstrap";
+
+function QuestionManagement() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default QuestionManagement
